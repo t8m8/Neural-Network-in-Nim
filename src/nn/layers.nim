d05aed879dfed441d937bdeccd77ee685b53bf43
t8m8 <t0m09488@gmail.com>
2017-05-21 12:52:18 +0900

    Clean layers.nim

--- src/nn/layers.nim
+++ src/nn/layers.nim
@@ -22,41 +22,56 @@ type
 proc `[]=`*(self: var Links, row, col: int, w: NNFloat) =
   self.weights[row, col] = w
 
-method forward*(self: Layer, incoming: Matrix[NNFloat]): Matrix[NNFloat] {.base.} =
+method forward*(self: Layer, incoming: Matrix[NNFloat]):
+    Matrix[NNFloat] {.base.} =
   assert false
 
-method backward*(self: Layer, outgoing: Matrix[NNFloat], above: Matrix[NNFloat]): Matrix[NNFloat] {.base.} =
+method backward*(self: Layer, outgoing: Matrix[NNFloat],
+    above: Matrix[NNFloat]): Matrix[NNFloat] {.base.} =
   assert false
 
+# ==============================================================================
 
 proc newIdentity*(): Identity {.noSideEffect.} =
   new(result)
 
-method forward*(self: Identity, incoming: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
+method forward*(self: Identity, incoming: Matrix[NNFloat]):
+    Matrix[NNFloat] {.noSideEffect.} =
   incoming
 
-method backward*(self: Identity, outgoing: Matrix[NNFloat], above: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
+method backward*(self: Identity, outgoing: Matrix[NNFloat],
+    above: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
   above
+
+# ==============================================================================
 
 proc newSigmoid*(): Sigmoid {.noSideEffect.} =
   new(result)
 
-method forward*(self: Sigmoid, incoming: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
+method forward*(self: Sigmoid, incoming: Matrix[NNFloat]):
+    Matrix[NNFloat] {.noSideEffect.} =
   incoming.transform((val: NNFloat) => 1.0 / (1.0 + exp(-val)))
 
-method backward*(self: Sigmoid, outgoing: Matrix[NNFloat], above: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
+method backward*(self: Sigmoid, outgoing: Matrix[NNFloat],
+    above: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
   outgoing.transform((val: NNFloat, row, col: int) =>
     val * (1.0 - val) * above[row, col]
   )
 
+# ==============================================================================
+
 proc newReLU*(): ReLU {.noSideEffect.} =
   new(result)
 
-method forward*(self: ReLU, incoming: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
+method forward*(self: ReLU, incoming: Matrix[NNFloat]):
+    Matrix[NNFloat] {.noSideEffect.} =
   incoming.transform((val: NNFloat) => max(0, val))
 
-method backward*(self: ReLU, outgoing: Matrix[NNFloat], above: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
+method backward*(self: ReLU, outgoing: Matrix[NNFloat],
+    above: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
   outgoing.transform((val: NNFloat) => ord(val > 0).NNFloat)
+
+# ==============================================================================
 
 proc newDense*(inputDim, outputDim: int, eps: NNFloat = 1e-4): Dense =
   new(result)
@@ -71,9 +86,10 @@ proc newDense*(inputDim, outputDim: int, weights: Matrix[NNFloat]): Dense =
   result.outputDim = outputDim
   result.weights = weights
 
-method forward*(self: Dense, incoming: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
+method forward*(self: Dense, incoming: Matrix[NNFloat]):
+    Matrix[NNFloat] {.noSideEffect.} =
   incoming * self.weights
 
-method backward*(self: Dense, outgoing: Matrix[NNFloat], above: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
+method backward*(self: Dense, outgoing: Matrix[NNFloat],
+    above: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
   above * self.weights.t
-
