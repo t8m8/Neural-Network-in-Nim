be4736b7cde06728b0c824d10bb5adfc9611d826
t8m8 <t0m09488@gmail.com>
2017-05-21 13:31:18 +0900

    Clean lossfuncs.nim

--- src/nn/lossfuncs.nim
+++ src/nn/lossfuncs.nim
@@ -9,33 +9,43 @@ type
   CrossEntropy = ref object of LossFunc
   BinaryCrossEntropy = ref object of LossFunc
 
-method loss*(self: LossFunc, output: Matrix[NNFloat], expected: Matrix[NNFloat]): Matrix[NNFloat] {.base.} =
+method loss*(self: LossFunc, output: Matrix[NNFloat],
+    expected: Matrix[NNFloat]): Matrix[NNFloat] {.base.} =
   assert false
 
-method backward*(self: LossFunc, output: Matrix[NNFloat], expected: Matrix[NNFloat]): Matrix[NNFloat] {.base.} =
+method backward*(self: LossFunc, output: Matrix[NNFloat],
+    expected: Matrix[NNFloat]): Matrix[NNFloat] {.base.} =
   assert false
 
-method predictFromProbs*(self: LossFunc, probs: Matrix[NNFloat]): Matrix[int] {.base.} =
+method predictFromProbs*(self: LossFunc, probs: Matrix[NNFloat]):
+    Matrix[int] {.base.} =
   assert false
+
+# ==============================================================================
 
 proc newCrossEntropy*(): CrossEntropy {.noSideEffect.} =
   new(result)
 
-method loss*(self: CrossEntropy, output: Matrix[NNFloat], expected: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
-  result = output.reduceRows(0.0, proc(acc, val: NNFloat, row, col: int): NNFloat =
+method loss*(self: CrossEntropy, output: Matrix[NNFloat],
+    expected: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
+  result = output.reduceRows(0.0, proc(acc, val: NNFloat, row, col: int):
+      NNFloat =
     if expected[row, col] > 0.0:
       acc - ln(val)
     else:
       acc
   )
 
-method backward*(self: CrossEntropy, output: Matrix[NNFloat], expected: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
+method backward*(self: CrossEntropy, output: Matrix[NNFloat],
+    expected: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
   result = output.transform((val: NNFloat, row, col) => val - expected[row, col])
 
-method predictFromProbs*(self: CrossEntropy, probs: Matrix[NNFloat]): Matrix[int] {.noSideEffect.} =
+method predictFromProbs*(self: CrossEntropy, probs: Matrix[NNFloat]):
+    Matrix[int] {.noSideEffect.} =
   result = probs.reduceRows(
     (0.0, 0),
-    proc(acc: tuple[max: NNFloat, maxIdx: int], val: NNFloat, row, col: int): tuple[max: NNFloat, maxIdx: int] =
+    proc(acc: tuple[max: NNFloat, maxIdx: int], val: NNFloat, row, col: int):
+        tuple[max: NNFloat, maxIdx: int] =
       if val > acc.max:
         result = (val, col)
       else:
@@ -44,21 +54,28 @@ method predictFromProbs*(self: CrossEntropy, probs: Matrix[NNFloat]): Matrix[int
     (val: tuple[max: NNFloat, maxIdx: int]) => val.maxIdx
   )
 
+# ==============================================================================
+
 proc newBinaryCrossEntropy*(): BinaryCrossEntropy {.noSideEffect.} =
   new(result)
 
-method loss*(self: BinaryCrossEntropy, output: Matrix[NNFloat], expected: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
-  result = output.reduceRows(0.0, proc(acc, val: NNFloat, row, col: int): NNFloat =
+method loss*(self: BinaryCrossEntropy, output: Matrix[NNFloat],
+    expected: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
+  result = output.reduceRows(0.0, proc(acc, val: NNFloat, row, col: int):
+      NNFloat =
     if expected[row, col] < 1e-5:
       -(1.0 - val).ln
     else:
       -val.ln
   )
 
-method backward*(self: BinaryCrossEntropy, output: Matrix[NNFloat], expected: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
-  result = output.transform((val: NNFloat, row, col) => val - expected[row, col])
+method backward*(self: BinaryCrossEntropy, output: Matrix[NNFloat],
+    expected: Matrix[NNFloat]): Matrix[NNFloat] {.noSideEffect.} =
+  result = output.transform(
+      (val: NNFloat, row, col) => val - expected[row, col])
 
-method predictFromProbs*(self: BinaryCrossEntropy, probs: Matrix[NNFloat]): Matrix[int] {.noSideEffect.} =
+method predictFromProbs*(self: BinaryCrossEntropy, probs: Matrix[NNFloat]):
+    Matrix[int] {.noSideEffect.} =
   result = probs.transform(proc(val: NNFloat): int =
     if val >= 0.5: 1
     else: 0
