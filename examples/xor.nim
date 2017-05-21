4b12faa9adfa630f89409dcf525e3d3a6be3d43d
t8m8 <t0m09488@gmail.com>
2017-05-22 05:03:54 +0900

    Fix xor example

--- examples/xor.nim
+++ examples/xor.nim
@@ -2,26 +2,26 @@ import nn
 import random
 
 proc genXorData(n: int): tuple[input: Matrix[float], output: Matrix[float]] =
-  var (input, output) = (newMat[float](n, 2), newMat[float](n, 1))
+  var (input, output) = (newMat[float](n, 2), newMat[float](n, 2))
   for i in 0..<n:
     input[i, 0] = random(2).float
     input[i, 1] = random(2).float
-    output[i, 0] = (input[i, 0].int xor input[i, 1].int).float
+    output[i] = oneHot[float](2, input[i, 0].int xor input[i, 1].int)
   result = (input, output)
 
 when isMainModule:
   var network = newNNBuilder()
     .add(newDense(2, 10))
     .add(newSigmoid())
-    .add(newDense(10, 1))
-    .add(newSigmoid())
-    .minimize(newBinaryCrossEntropy())
+    .add(newDense(10, 2))
+    .add(newSoftmax())
+    .minimize(newCrossEntropy())
     .optimize(newSGD(0.1))
     .build()
 
   var
     (trainInput, trainOutput) = genXorData(1000000)
-    epochs = 3 
+    epochs = 3
     batchSize = 64
     options = newOptions(epochs, batchSize)
 
